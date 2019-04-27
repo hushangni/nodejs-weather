f2419e52a24298c4a9850e8ee2ba4cc24deac79e
Shangni <shangnihu@gmail.com>
2019-04-27 17:18:06 -0400

    Added front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log('client side JS loaded');
+console.log("client side JS loaded");
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    })
+})
